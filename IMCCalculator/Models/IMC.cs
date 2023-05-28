295f48b59ac8a7ee4dd3b15aed070618f5e321ad
Luis Ferreira <lg_pinguim@hotmail.com>
2023-05-28 16:22:37 +0100

    Add project files.

---
+++ IMCCalculator/Models/IMC.cs
@@ -0,0 +1,20 @@
+﻿namespace IMCCalculator.Models
+{
+    public class IMC
+    {
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public double Resultado { get; set; }
+
+        public void CalculaIMC()
+        {
+            if (Peso ==0 || Altura ==0)
+            {
+                Resultado = 0;
+            }
+
+            Resultado = Math.Round(Peso / Math.Pow(Altura, 2),2);
+
+        }
+    }
+}
